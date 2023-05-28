1153c2a69a8afb5bebd3a9ee782645fde44d75fd
cbranam18 <cbranam18@gmail.com>
2023-05-27 23:10:27 -0400

    Changed names and completed challenge

--- scripts/app1-03.R
+++ scripts/app1-03.R
@@ -8,27 +8,37 @@
 #Create six variables tht hold length values
 #first and second are 25 and 30
 
-V1= 25;
-V2= 30;
+  V_1= 25;
+  V_2= 30;
 
 #third and fourth are random between 20 and 30 (Equal Chance)
 
-  V3= sample(20:30, size=1);
-  V4= sample(20:30, size=1);
+  V_3= sample(20:30, size=1);
+  V_4= sample(20:30, size=1);
   
 #fifth and sixth values are randomly picked from a normdist, mean = 25 sd=2   
 
-  V5= rnorm(n=1, mean=25, sd=2);
-  V6= rnorm(n=1, mean=25, sd=2);
+  V_5= rnorm(n=1, mean=25, sd=2);
+  V_6= rnorm(n=1, mean=25, sd=2);
 
 #Calculate mean, variance, and sd of the six variables
 
-  Vmean= (V1 + V2+ V3 + V4 + V5 + V6)/6;
+  V_mean= (V_1 + V_2+ V_3 + V_4 + V_5 + V_6)/6;
 
-  Vvar= ((V1-Vmean)^2 + (V2-Vmean)^2 + (V3-Vmean)^2 + (V4-Vmean)^2 + (V5-Vmean)^2 + 
-  (V6-Vmean)^2) / (6);
+  V_var= ((V_1-V_mean)^2 + (V_2-V_mean)^2 + (V_3-V_mean)^2 + (V_4-V_mean)^2 + 
+  (V_5-V_mean)^2 + (V_6-V_mean)^2) / (6);
 
-  Vsd= (Vvar)^(1/2)
+  V_sd= (V_var)^(1/2)
   
-}
+
+  
+### Challenge ###
+  
+# Pick a random two digit number between 0 and 1
+  #Sample a number 1-100 and divide by 100
+  
+  C_1= sample(0:100, size = 1);
+  C_2= C_1 / 100;
+  
+  }
 
